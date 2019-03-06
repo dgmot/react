93379b338c12a6c3cd1f0f817d6ce51515fcc544
dgmot <gabifhn@gmail.com>
2019-03-06 19:12:24 +0100

    Updates

---
+++ precache-manifest.10b681184817cf0e9a202d9fbcbbbea9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "95f9effbb71490554411",
+    "url": "/react/static/css/main.46a16e11.chunk.css"
+  },
+  {
+    "revision": "95f9effbb71490554411",
+    "url": "/react/static/js/main.95f9effb.chunk.js"
+  },
+  {
+    "revision": "94c377705e3d70f3c42d",
+    "url": "/react/static/js/1.94c37770.chunk.js"
+  },
+  {
+    "revision": "bf2515a31126aef78a60",
+    "url": "/react/static/js/runtime~main.bf2515a3.js"
+  },
+  {
+    "revision": "af160f5fd54190c3ea4ff3257013fe4c",
+    "url": "/react/index.html"
+  }
+];
